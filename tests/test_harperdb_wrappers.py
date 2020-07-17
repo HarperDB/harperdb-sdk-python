319eddbc16c1246314eb58cd41a436a93dba07ac
Tom Young <shadetreemetalworks@gmail.com>
2020-07-17 14:04:16 -0600

    use tearDown method to clean up responses

--- tests/test_harperdb_wrappers.py
+++ tests/test_harperdb_wrappers.py
@@ -12,6 +12,11 @@ class TestHarperDBWrapper(harperdb_testcase.HarperDBTestCase):
         """
         self.db = harperdb.wrappers.HarperDBWrapper(self.URL)
 
+    def tearDown(self):
+        """ This method is called after each test.
+        """
+        responses.reset()
+
     def test_harperdb_is_subscriptable_by_schema_name(self):
         """ HarperDB is subscriptable by schema name.
         """
@@ -126,6 +131,11 @@ class TestHarperDBRecord(harperdb_testcase.HarperDBTestCase):
         self.record = harperdb.wrappers.HarperDBRecord(
             table=self.table,
             hash_value=self.RECORDS[0]['id'])
+
+    def tearDown(self):
+        """ This method is called after each test.
+        """
+        responses.reset()
 
     def test_create_harperdbrecord_with_kwargs(self):
         """ Create an instance of HarperDBTable with keyowrd args.
@@ -289,6 +299,11 @@ class TestHarperDBSchema(harperdb_testcase.HarperDBTestCase):
         self.schema = harperdb.wrappers.HarperDBSchema(
             name='test_schema_1',
             database=self.db)
+
+    def tearDown(self):
+        """ This method is called after each test.
+        """
+        responses.reset()
 
     def test_create_harperdbschema_with_kwargs(self):
         """ Create an instance of HarperDBSchema with keyword args.
@@ -477,6 +492,11 @@ class TestHarperDBTable(harperdb_testcase.HarperDBTestCase):
             name=self.DESCRIBE_TABLE['name'],
             schema=self.schema,
             hash_attribute='id')
+
+    def tearDown(self):
+        """ This method is called after each test.
+        """
+        responses.reset()
 
     @responses.activate
     def test_create_harperdbtable_with_kwargs(self):
